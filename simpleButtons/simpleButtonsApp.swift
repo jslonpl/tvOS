05078bf02de31dcf7c6797ce16e264f96ac9e10f
jslonpl <jslonpl@gmail.com>
2022-02-04 23:03:48 +0100

    Initial Commit

---
+++ simpleButtons/simpleButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleButtonsApp.swift
+//  simpleButtons
+//
+//  Created by Jakub Słoń on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct simpleButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
